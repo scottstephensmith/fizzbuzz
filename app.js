1ba4c778f57350a1e59b899056aad47be55e6e36
Scott Smith <scottstephensmith@gmail.com>
2014-07-25 14:57:16 -0400

    still working, this version has the alert
    it's now multiplying user input by x10

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 $( document ).ready(function() {
 
-var userNumber = prompt("Please enter a number:");
-var realNumber = parseInt(userNumber);
 
-var input = function(realNumber) {
-	for (number = 1; number < realNumber; number ++){
-		if (userNumber == NaN){
-			console.log("not a valid number")
-			alert("Please enter a real number!");
-		}
-		else if ((number % 3 == 0) && (number % 5 == 0)) {
+var userNumber = prompt("Please enter a number:");
+	while (isNaN(userNumber) === true) {
+		console.log("invalid number entered");
+		userNumber = prompt("Try again with a real number this time:");
+	}
+	console.log("number inputed by user was: " + userNumber);
+	
+
+/*var valdiate = function(userNumber){
+	if (userNumber == NaN) {
+		alert("Please enter a positive integer!");
+	}
+	else {
+		parseInt(userNumber);
+		userNumber = realNumber;
+	}
+}
+
+ var realNumber = parseInt(userNumber); */
+
+var calcIt = function(number) {
+	for (number = 1; number < (userNumber + 1); number ++){
+		if ((number % 3 == 0) && (number % 5 == 0)) {
 			console.log("fizzbuz");
 			$("body").append('fizzbuzz<br>');
 		}
@@ -27,6 +41,5 @@ var input = function(realNumber) {
 		}
 }
 }
-
-input(realNumber);
+calcIt(userNumber);
 });
