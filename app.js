7d89eaddc0022eb8c2f97f57bee03025f7e264d9
scottstephensmith <ScottStephenSmith@gmail.com>
2014-07-24 22:49:46 -0400

    trying to refactor... without testing

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 $( document ).ready(function() {
+var userNumber = prompt("Please enter a number:");
+userNumber();
 
-for (number = 1; number < 101; number ++){
-	if ((number % 3 == 0) && (number % 5 == 0)) {
-		console.log("fizzbuz");
-		$("body").append('fizzbuzz<br>');
-	}
-	else if ( number%3 == 0){
-		console.log("fizz");
-		$("body").append('fizz<br>');
-	}
-	else if (number%5 == 0){
-		console.log("buzz");
-		$("body").append('buzz<br>');
-	}
-	else {
-		console.log(number);
-		$("body").append(number + "<br>");
-	}
+var input = function(userNumber) {
+	for (number = 1; number < +userNumber; number ++){
+		if (+number == NaN){
+			alert("Please enter a real number!");
+			userNumber();
+		}
+		else if ((number % 3 == 0) && (number % 5 == 0)) {
+			console.log("fizzbuz");
+			$("body").append('fizzbuzz<br>');
+		}
+		else if ( number%3 == 0){
+			console.log("fizz");
+			$("body").append('fizz<br>');
+		}
+		else if (number%5 == 0){
+			console.log("buzz");
+			$("body").append('buzz<br>');
+		}
+		else {
+			console.log(number);
+			$("body").append(number + "<br>");
+		}
 }
+
 
 });
