ba7401792706d5df19c7213a20c92f65e24e46b4
scottstephensmith <ScottStephenSmith@gmail.com>
2014-07-24 23:07:12 -0400

    using parsInt to get integer

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
+
 var userNumber = prompt("Please enter a number:");
+var realNumber = parseInt(userNumber);
 
 var input = function(userNumber) {
-	for (number = 1; number < +userNumber; number ++){
-		if (+number == NaN){
+	for (number = 1; number < realNumber; number ++){
+		if (realNumber === NaN){
 			alert("Please enter a real number!");
-			userNumber();
 		}
 		else if ((number % 3 == 0) && (number % 5 == 0)) {
 			console.log("fizzbuz");
